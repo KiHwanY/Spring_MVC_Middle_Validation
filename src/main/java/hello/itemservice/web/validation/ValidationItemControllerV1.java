8d5e04d449fafac61130ada53ada77ec6fb8257e
KiHwan Yun <drg2524@naver.com>
2023-12-20 10:38:43 +0900

    V1 Validation 강의 수강 완료

--- src/main/java/hello/itemservice/web/validation/ValidationItemControllerV1.java
+++ src/main/java/hello/itemservice/web/validation/ValidationItemControllerV1.java
@@ -48,6 +48,22 @@ public class ValidationItemControllerV1 {
         //검증 오류 결과를 보관
         Map<String, String> errors = new HashMap<>();
 
+        //import org.springframework.util.StringUtils;
+        // StringUtils 클래스는 자바의 String 클래스가 제공하는 문자열 관련 기능을 강화한 클래스이다.
+        // 유용한 메서들로는 아래 참고.
+
+        // isEmpty() -> null체크도 해주고, 길이가 0이 아닌지 체크한다. 하지만 이것보다 StringUtils를 쓸 때는 hasText()를 권장한다.
+        // isEmpty() 를 사용할 때는
+        // 1. 꼭 String이 아닌 객체를 null 체크를 하고 싶을 때 ex) Long,Integer
+        // 2. ObjectUtils 를 쓸 때
+
+        // hasText() -> 문자열 유효성 검증 유틸 메서드이다.
+        // 1. null 인 것.
+        // 2. 길이가 0인 것.
+        // 3. 공백("" or " ")인 것
+        // 이 문자열에 하나라도 포함되었다면 false이다.
+
+        // 결론적으로 사용하는 이유는 둘 다 파라미터 값으로 null을 주더라도 절대 NullPointException을 발생시키지 않는다.
         //검증 로직
         if (!StringUtils.hasText(item.getItemName())) {
             errors.put("itemName", "상품 이름은 필수입니다.");
