8c0c6e64fc34386efa42d4fa839663a15d9a2607
KiHwan Yun <drg2524@naver.com>
2023-12-20 16:40:15 +0900

    BindingResult 2 챕터 정리

--- src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
+++ src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
@@ -81,7 +81,7 @@ public class ValidationItemControllerV2 {
     * */
 
 
-    @PostMapping("/add")
+//    @PostMapping("/add")
     public String addItemV1(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
 //      FieldError 생성자 요약
 //      public FieldError(String objectName , String field , String defaultMessage){}
@@ -134,7 +134,7 @@ public class ValidationItemControllerV2 {
         return "redirect:/validation/v2/items/{itemId}";
     }
 
-//    @PostMapping("/add")
+    @PostMapping("/add")
     public String addItemV2(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
 
         //검증 로직
@@ -147,6 +147,27 @@ public class ValidationItemControllerV2 {
         if (item.getQuantity() == null || item.getQuantity() >= 9999) {
             bindingResult.addError(new FieldError("item", "quantity", item.getQuantity(), false, null ,null, "수량은 최대 9,999 까지 허용합니다."));
         }
+/*    objectName : 오류가 발생한 객체의 이름
+    * field : 오류 필드
+    * rejectValue : 사용자가 입력한 값(거절된 값)
+    * bindingFailure : 타입 오류 같은 바인딩 실패인지, 검증 실패인지 구분 값
+    * codes : 메시지 코드
+    * arguments : 메시지에서 사용하는 인자
+    * defaultMessage : 기본 오류 메시지*/
+
+        /* [오류 발생시 사용자 입력값 유지]
+        * new FieldError("item", "price", item.getPrice(), false, null, null, "가격은 1,000 ~ 1,000,000 까지 허용합니다."
+        *
+        * 사용자의 입력 데이터가 컨트롤러의 @ModelAttribute에 바인딩되는 시점에 오류가 발생하면 모델 객체에 사용자 입력 값을 유지하기 어렵다.
+        * 예를 들어서 가격에 숫자가 아닌 문자가 입력된다면 가격은 Integer 타입이므로 문자를 보관할 수 있는 방법이 없다.
+        * 그래서 오류가 발생한 경우 사용자 입력 값을 보관하는 별도의 방법이 필요하다.
+        * 그리고 이렇게 보관한 사용자 입력 값을 검증 오류 발생시 화면에 다시 출력하면 된다.
+        *
+        * FieldError 는 오류 발생 시 사용자 입력 값을 저장하는 기능을 제공한다.
+        *
+        * 여기서 rejectValue 가 바로 오류 발생시 사용자 입력 값을 저장하는 필드다.
+        * bindingFailure 는 타입 오류 같은 바인딩이 실패했는지 여부를 적어주면 된다. 여기서는 바인딩이 실패한 것은 아니기 때문에 false 를 사용한다.
+        *  */
 
         //특정 필드가 아닌 복합 룰 검증
         if (item.getPrice() != null && item.getQuantity() != null) {
