3ba6c19f8d61dad4543badac9d6e3b47e580aa60
KiHwan Yun <drg2524@naver.com>
2023-12-21 15:32:42 +0900

    Bean Validation - Object Error 섹션 정리

--- src/main/java/hello/itemservice/domain/item/Item.java
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -8,8 +8,22 @@ import javax.validation.constraints.Max;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
-
+/*
+*  [Bean Validation - Object Error]
+*
+*  Bean Validation 에서 특정 필드(FieldError)가 아닌 해당 Object 관련 오류(ObjectError)는 어떻게 처리할 수 있을까?
+*  다음과 같이(@ScriptAssert)를 사용하면 된다.
+*
+*
+*  [@ScriptAssert 권장하지 않는 이유]
+*  실제 사용해보면 제약이 많고 복잡하다.
+*  실무에서는 검증 기능이 해당 객체의 범위를 넘어서는 경우들이 종종 등장하는데, 그런 경우 대응이 어렵다.
+*
+*  따라서 Object Error(Global Error) 의 경우 @ScriptAssert 을 억지로 사용하는 것보다는 Object Error 관련 부분만
+*  직접 java code 로 작성하는 것을 권장한다.
+* */
 @Data
+//@ScriptAssert(lang = "javascript" , script="_this.price * _this.quantity >= 10000")
 public class Item {
     /*
     *  [Bean Validation 이란?]
@@ -26,6 +40,11 @@ public class Item {
         @NotNull : null 을 허용하지 않는다.
         @Range(min = 1000, max = 1000000) : 범위 안의 값이어야 한다.
         @Max(9999) : 최대 9999까지만 허용한다.
+
+    * [Bean Validation 메시지 찾는 순서]
+    *  - 생성된 메시지 코드 순서대로 messageSource 에서 메시지 찾기
+    *  - 애노테이션 message 속성 사용 -> @NotBlank(message = "공백! {0}")
+    *  - 라이브러리가 제공하는 기본 값 사용 -> 공백일 수 없습니다.
     * */
 
     @NotNull(groups = UpdateCheck.class) //수정 요구사항 추가
